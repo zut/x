026332f660f7c3a7b81df0ac3d471893cf07e4c3
zut <zut@dai3.com>
2020-10-16 15:45:09 +0800

    Skip
    Skip

--- example/t_xlog_example.go
+++ example/t_xlog_example.go
@@ -9,8 +9,10 @@ import (
 
 func f2() {
 	defer end()
-	xlog.Debug(1, "Skip1")
+	xlog.Debug("111 Skip1")
+	xlog.Debug(1, "1 Skip1")
 	xlog.Info(2, "Skip2")
+	xlog.Error(3, "Skip1")
 	xlog.Warning(3)
 	xlog.Error(g.Map{"1": 123123, "222": "abc"})
 	//xlog.Panic(5)
